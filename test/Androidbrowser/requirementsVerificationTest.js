7405b2eedbe970a9d324be83f5933c9c7f720cf2
Pallavi Srivastava <vikas@vtestcorp.com>
2021-10-08 10:28:52 +0530

    Requirements page verification and unlock fee payments module updated

--- test/Androidbrowser/requirementsVerificationTest.js
+++ test/Androidbrowser/requirementsVerificationTest.js
@@ -5,9 +5,9 @@ describe('Requirements Verification', async () => {
         await driver.closeApp()
         await driver.launchApp()
     });
-    it('TC_01 Validate all requirements created userLogin are displayed', async () => {
+    /*it('TC_01 Validate all requirements created userLogin are displayed', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         console.log("Buyer login successfully")
         await base.swipeUp()
@@ -20,7 +20,7 @@ describe('Requirements Verification', async () => {
     });
     it('TC_02 Verify requirements score is a display', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         console.log("Buyer login successfully")
         await base.swipeUp()
@@ -34,7 +34,7 @@ describe('Requirements Verification', async () => {
     });
     it('TC_03 Verify requirements name is editable', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         await base.swipeUp()
         await (await requirementsVerificationPage.my_Requirements).click()
@@ -45,7 +45,7 @@ describe('Requirements Verification', async () => {
     });
     it('TC_04 Verify Add New Requirements link is available', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         console.log("Buyer login successfully")
         await base.swipeUp()
@@ -55,10 +55,10 @@ describe('Requirements Verification', async () => {
         expect(await requirementsVerificationPage.Add_New_Requirement).toBeDisplayed()
         console.log("+ Add New Requirements link is available")
         console.log(await (await requirementsVerificationPage.Add_New_Requirement).getText())
-    });
+    });*/
     it('TC_05 Verify user should be able to edit Do You Wish to Exchange Your Vehicle option', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         console.log("Buyer login successfully")
         await base.swipeUp()
@@ -71,14 +71,16 @@ describe('Requirements Verification', async () => {
         console.log(await (await requirementsVerificationPage.Score_Count).getText())
         expect(await requirementsVerificationPage.requirements).toBeDisplayed()
         console.log(await (await requirementsVerificationPage.requirements).getText())
-        expect(await requirementsVerificationPage.below).toBeDisplayed()
-        console.log(await (await requirementsVerificationPage.below).getText())
+        expect(await requirementsVerificationPage.Price_Range).toBeDisplayed()
+        console.log(await (await requirementsVerificationPage.Price_Range).getText())
         expect(await requirementsVerificationPage.Used).toBeDisplayed()
         expect(await requirementsVerificationPage.any).toBeDisplayed()
+        expect(await requirementsVerificationPage.best_Matches).toBeDisplayed()
+        expect(await requirementsVerificationPage.my_Matches).toBeDisplayed()
     });
-    it('TC_06 Verify payments status of requirements are display ', async () => {
+    /*it('TC_06 Verify payments status of requirements are display ', async () => {
         await requirementsVerificationPage.chromeConditions()
-        browser.url('/')
+        browser.url('/user/login')
         await requirementsVerificationPage.loginAsBuyer()
         console.log("Buyer login successfully")
         await base.swipeUp()
@@ -87,5 +89,5 @@ describe('Requirements Verification', async () => {
         expect(await requirementsVerificationPage.Unlock_Fee_Payment).toBeDisplayed()
         console.log("Payments status of requirements are displayed")
         console.log(await (await requirementsVerificationPage.Unlock_Fee_Payment).getText())
-    });
+    });*/
 });
---
+++ test/Androidbrowser/unlockFeePaymentTest.js
@@ -0,0 +1,37 @@
+
+const { swipeUp } = require("../../BaseClass/base");
+const base = require("../../BaseClass/base");
+const unlockFeePaymentPage = require("../../Pages/Androidbrowser/unlockFeePaymentPage");
+
+describe('Unlock fee payments', async () => {
+    beforeEach(async () => {
+        await driver.closeApp()
+        await driver.launchApp()
+    });
+    it('TC_01 Verify the token amount for car is 999', async () => {
+        await unlockFeePaymentPage.chromeConditions()
+        browser.url('/user/login')
+        await unlockFeePaymentPage.loginAsBuyer()
+        await base.swipeUp()
+        await (await unlockFeePaymentPage.my_Requirements).click()
+        await (await unlockFeePaymentPage.new_Requirement).click()
+        await unlockFeePaymentPage.createRForCar()
+        await (await unlockFeePaymentPage.best_Matches).click()
+        expect(await unlockFeePaymentPage.tokenForCar).toBeDisplayed()
+        console.log("Token amount for car:")
+        console.log(await (await unlockFeePaymentPage.tokenForCar).getText())
+    });
+    /*it('TC_02 Verify the token amount for car is 499', async () => {
+        await unlockFeePaymentPage.chromeConditions()
+        browser.url('/user/login')
+        await unlockFeePaymentPage.loginAsBuyer()
+        await base.swipeUp()
+        await (await unlockFeePaymentPage.my_Requirements).click()
+        await (await unlockFeePaymentPage.new_Requirement).click()
+        await unlockFeePaymentPage.createRForCar()
+        await (await unlockFeePaymentPage.best_Matches).click()
+        expect(await unlockFeePaymentPage.tokenForCar).toBeDisplayed()
+        console.log("Token amount for car:")
+        console.log(await (await unlockFeePaymentPage.tokenForCar).getText())
+    });*/
+});
