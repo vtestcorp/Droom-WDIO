b7ce891dd96273c67f1b720644152c44ba19e1b9
punamvtest <poonam@vtestcorp.com>
2021-12-21 20:11:20 +0530

    used bike testcases

--- Pages/Android/UsedBikePage.js
+++ Pages/Android/UsedBikePage.js
@@ -27,6 +27,31 @@ class usedbikepage{
     get trimoption() {return base.getByText("220CC")}
     get bikeName() {return base.getByResourceId("in.droom:id/makeModelYearTrimTV")}
     get addmoreVehicle() {return base.getByResourceId("in.droom:id/addMoreVehicleBTN")}
+    get droomSuggest() {return base.getByResourceId("in.droom:id/droomSuggestBTN")}
+    get heading2() {return base.getByText("Get Personalised Suggestion")}
+    get priceRange() {return base.getByText("Price Range")}
+    get bodyType() {return base.getByResourceId("in.droom:id/bodyTypeHeadingTV")}
+    get selecttype() {return base.getByText("Sports")}
+    get yearRange() {return base.getByText("Year Range")}
+    get selectyear() {return base.getByText("2018 & Above")}
+    get heading3() {return base.getByText("Now Share Your Dream Vehicle Basic Details")}
+    get extcolor() {return base.getByResourceId("in.droom:id/txt_select_exterior_color")}
+    get color() {return base.getByResourceId("in.droom:id/smb_CardCV")}
+    get heading4() {return base.getByText("Last Step to Go! Few More Details")}
+    get doyouneedoption() {return base.getByText("Do You Also Need?")}
+    get loan() {return base.getByResourceId("in.droom:id/txt_smb_loan")}
+    get loantoggle() {return base.getByResourceId("in.droom:id/loan_toggle")}
+    get heading5() {return base.getByText("Great! More Information Gives Best Results")}
+    get submitBtn() {return base.getByText("Submit")}
+    get quickyBuy() {return base.getByText("How Quickly Do You Want to Buy?")}
+    get duration() {return base.getByResourceId("in.droom:id/radio_one_four_week")}
+    get budget() {return base.getByText("What Is Your Budget?")}
+    get priceslider() {return base.getByResourceId("in.droom:id/priceRangeSlider")}
+    get submitViewMatch() {return base.getByText("Submit & View all Matches")}
+    get allrequirement() {return base.getByText("View All Requirements")}
+    get priceRange() {return base.getByText("Below 1 Cr")}
+    get kmDriven() {return base.getByText("KM Driven Range")}
+    get kmSlider() {return base.getByResourceId("in.droom:id/kmDrivenSlider")}
 
 
     async selectBikeVehicle() {
@@ -40,6 +65,18 @@ class usedbikepage{
         await (await this.trimoption).click()
         console.log("selected Bike as per requirement")
     }
+
+    async droomSuggestBike(){
+        await (await this.selecttype).click()
+        console.log("body type selected")
+        base.swipeUp()
+        expect(await this.yearRange).toBeDisplayed()
+        console.log('Year Range Displayed')
+        await (await this.selectyear).click()
+        console.log("Year selected")
+        await (await this.nextBtn).click()
+        console.log("clicked on Next button")
+       }
 
 }
 
