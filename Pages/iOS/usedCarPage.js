912d64d17d999f5efda6db77ffe0d0871a3430e8
vtestcorp1@gmail.com <vtestcorp1@gmail.com>
2021-12-22 14:14:08 +0530

    iOS files for Vehicle Insurance and Used Car

--- Pages/iOS/usedCarPage.js
+++ Pages/iOS/usedCarPage.js
@@ -13,21 +13,20 @@ class Usedcar {
     get scooter() { return base.getByAccessibilityID('Scooter') }
     get back(){return base.getByAccessibilityID('arrow left')}
     get carimg() { return base.getByAccessibilityID('transportation') }
-
     get newButton() { return base.getByClassChain('**/XCUIElementTypeButton[`label == "New"`]') }
     get location() { return base.getByAccessibilityID('Select Your Location') }
     get searchMagnifier(){return base.getByAccessibilityID('buySearch')}
     get nextButton() { return base.getByClassChain('**/XCUIElementTypeStaticText[`label == "Next"`]') }
-   // get nextBtn2(){return base.getByAccessibilityID('**/XCUIElementTypeStaticText[`label == "Next"`]')}
     get selectCarHeading(){return base.getByAccessibilityID("Let's Find Your Dream Car")}
     get currentLocation() { return base.getByClassChain('**/XCUIElementTypeButton[`label == "pune"`]')}
     get errorMsg() { return base.getByClassChain('**/XCUIElementTypeStaticText[`label == "Please Select Condition"`][1]')}
-    get vehicleDetails() { return base.getByResourceId('Enter Your Vehicle Details') }
+    get vehicleDetails() { return base.getByAccessibilityID('Enter Your Vehicle Details') }
     get selectVehicle(){return base.getByAccessibilityID('**/XCUIElementTypeStaticText[`label == "Select Vehicle"`]')}
     get searchTextField(){return base.getByClassChain('**/XCUIElementTypeTextField[`value == "E.g Maruti Suzuki Swift LDI"`]')}
     get selectSearchResult(){return base.getByAccessibilityID('Select Search Result')}
     get doneBtn(){return base.getByAccessibilityID('Done')}
     get cancelBtn(){return base.getByAccessibilityID('Cancel')}
+    get addmoreVerhicle(){return base.getByClassChain('**/XCUIElementTypeStaticText[`label == " add more Vehicle"`]')}
     get swift() { return base.getByResourceId("in.droom:id/search_view") }
     get selectSwift() { return base.getByTextContains("Maruti Suzuki") }
     get sports(){return base.getByAccessibilityID('Sports')}
@@ -54,7 +53,7 @@ get fortEcoSport(){return base.getByClassChain('**/XCUIElementTypeStaticText[`la
     get submitR() { return base.getByResourceId("in.droom:id/menu_my_requirement")}
     get droomSugg() { return base.getByClassChain('**/XCUIElementTypeStaticText[`label == "Let Droom Suggest for you"`]') }
     get lookingFor() { return base.getByText("Price Range") }
-    get priceRange() { return base.getByTextContains("Price Range")}
+    get priceRange() { return base.getByAccessibilityID("Price Range")}
     get body() { return base.getByText("Body Type") }
     get age() { return base.getByText("Year Range") }
     get budget() { return base.getByText("What Is Your Budget?") }
@@ -83,10 +82,11 @@ get fortEcoSport(){return base.getByClassChain('**/XCUIElementTypeStaticText[`la
     get oneLastStepBeforeMatch(){return base.getByAccessibilityID('One Last Step before we Get Matches')}
     get kmsSlider(){return base.getByClassChain('**/XCUIElementTypeOther[`label == "kms_driven, Maximum value in slider, Minimum value in slider, Right Handle, Left Handle"`]')}
     get extColor() { return base.getByResourceIdNText("in.droom:id/txt_km_driven","Red") }
-    get transmissionType() { return base.getByResourceIdNText('Transmission Type') }
+    get transmissionType() { return base.getByAccessibilityID('Transmission Type') }
     get manualTransmission(){return base.getByAccessibilityID('Manual')}
     get automaticTransmission() { return base.getByAccessibilityID('Automatic') }
     get flexibleTransmission() { return base.getByResourceId("in.droom:id/radio_flexible_for_transmission_RV") }
+    get buy(){returnbase.getByAccessibilityID('Buy')}
     get submit() { return $('//span[text()="Submit "]') }
     get basicDetails() { return $('//h5[text()="Now Share Your Dream Vehicle Basic Details"]') }
     get backBtn() { return $('//button[@class="s-MuiButtonBase-root s-MuiIconButton-root"]//span[@class="s-MuiIconButton-label"]//*[local-name()="svg"]') }
@@ -97,8 +97,9 @@ get fortEcoSport(){return base.getByClassChain('**/XCUIElementTypeStaticText[`la
     get service() { return $('(//div[@class="r-MuiGrid-root r-r25 r-MuiGrid-item r-MuiGrid-grid-xs-12 r-MuiGrid-grid-sm-6 r-MuiGrid-grid-md-3"])[1]') }
     get needToBuy() { return $('(//div[@class="r-MuiGrid-root r-r25 r-MuiGrid-item r-MuiGrid-grid-xs-12 r-MuiGrid-grid-sm-6 r-MuiGrid-grid-md-3"])[2]') }
     get exchangeV() { return $('(//div[@class="r-MuiGrid-root r-r25 r-MuiGrid-item r-MuiGrid-grid-xs-12 r-MuiGrid-grid-sm-6 r-MuiGrid-grid-md-3"])[2]') }
-    get howQuicklyUwantBuy(){return base.getByAccessibilityID('How Quickly Do You Want to Buy?')}
-    get withinFourDay() { return $('**/XCUIElementTypeStaticText[`label == "Within 4 Days"`]') }
+    get howQuicklyUwantBuy(){return base.getByAccessibilityID("How Quickly Do You Want to Buy?")}
+    get withinFourDay() { return base.getByClassChain('**/XCUIElementTypeStaticText[`label == "Within 4 Days"`]') }
+    get doUwishExchange(){return base.getByAccessibilityID('Do You Wish to Exchange Your Vehicle? (optional)')}
     get exchange() { return base.getByClassChain('(//span[@class="s-MuiIconButton-label"])[12]') }
     get kms() { return $('//input[@id="exchange_kms_driven"]') }
     get records() { return $('//h3[@class="bm-MuiTypography-root bm-bm80 bm-MuiTypography-h3 bm-MuiTypography-noWrap"]') }
@@ -113,132 +114,6 @@ get fortEcoSport(){return base.getByClassChain('**/XCUIElementTypeStaticText[`la
     get Kmsbar() { return $('(//div[@class="noUi-connect"])[2]') }
     get kmsrange() { return base.getByAccessibilityID('Km Driven Range') }
     get exchangeVehicle() { return $('//p[text()="Do You Wish to Exchange Your Vehicle"]') }
-    
-    async submitRequirement() {
-        await base.scrollToTextContain("My Requirements")
-        await (await this.myReq).click()    
-        await (await this.newRequirements).click()
-    }
-    async carCondition() {
-        
-        await (await this.car).touchAction('tap') 
-        await (await this.usedButton).touchAction('tap') 
-    }
-    async clickOnNext() {
-        await (await this.nextButton).click()
-    }
-    async selectCity() {
-        await (await this.errorMsg).waitForExist({ timeout: 15000 })
-    }
-    async cLocation() {
-        await (await this.currentLocation).click()
-        await (await this.currentLocation).waitForExist({ timeout: 5000 })
-        await (await this.newButton).touchAction('tap')
-        await (await this.usedButton).touchAction('tap')
-    }
-    async selectNewBtn() {
-        await (await this.newButton).click()
-    }
-    async selectUsedBtn() {
-        await (await this.used).click()
-    }
-    async waitForVehicleDetails() {
-        await (await this.vehicleDetails).waitForExist({ timeout: 5000 })
-    }
-    async selectCar() {
-        await (await this.swift).click()
-        await (await this.swift).addValue("Maruti Suzuki swift")
-        await driver.pause(5000)
-        await driver.pressKeyCode(20)//down arrow
-        await driver.pressKeyCode(20)//down arrow
-        await driver.pressKeyCode(20)//down arrow
-        await driver.pause(2000)
-        await driver.pressKeyCode(66)//Enter
-        await driver.pause(2000)
-        await driver.hideKeyboard()
-    }
-    async deleteCarRecord() {
-        await (await this.closeRecord).click()
-    }
-    async slectFromDropDown() {
-        try {
-            //await (await this.addBtn).click()  
-        } catch (error) {       
-        } 
-        await (await this.selectFrom).touchAction('tap')
-        await (await this.make).waitForExist({ timeout: 5000 })
-    }
-    async selectVehicle() {
-        await (await this.make).touchAction('tap')
-        await (await this.selectMake).touchAction('tap')
-        //await driver.pause(2000)
-        await (await this.model).touchAction('tap')
-        await (await this.selectModel).waitForExist()
-        await (await this.selectModel).touchAction('tap')
-        //await driver.pause(2000)
-        await (await this.year).touchAction('tap')
-        await (await this.selectyear).waitForExist()
-        await (await this.selectyear).touchAction('tap')
-        //await driver.pause(2000)
-        await (await this.trim).touchAction('tap')
-        await (await this.selectTrim).waitForExist()
-        await (await this.selectTrim).touchAction('tap')
-        await driver.pause(2000)
-    }
-    async basicDetailsDisplay() {
-        await (await this.basicDetails).waitForExist({ timeout: 5000 })
-    }
-    async clickOnBack() {
-        await (await this.backBtn).click()
-        await (await this.carCancel).click()
-    }
-    async letDroomSuggest() {
-        await (await this.droomSugg).click()
-        await (await this.lookingFor).waitForDisplayed({ timeout: 5000 })
-    }
-    async clickOnGoBack() {
-        await (await this.goBack).click()
-        await (await this.vehicleDetails).waitForExist({ timeout: 5000 })
-    }
-    async selectPetrol() {
-        await (await this.petrol).click()
-    }
-    async clickOnSubmit() {
-        await (await this.submitAndView).click()
-    }
-    async clickOngetRequirements() {
-        await (await this.requirements).click()
-    }
-    async clickOnSubmitRequirements() {
-        await (await this.newRequirements).click()
-    }
-    async selectColor() {
-        await (await this.redColor).touchAction('tap')
-    }
-    async selectAutomatic() {
-        await(await this.flexibleTransmission).scrollIntoView()
-        await(await this.flexibleTransmission).touchAction('tap')
-        await (await this.automatic).touchAction('tap')
-    }
-    async selectDoYouAlsoNeed() {
-        await (await this.lastStep).waitForExist({ timeout: 5000 })
-        await (await this.insurance).touchAction('tap')
-        await (await this.loan).touchAction('tap')
-        await (await this.RCTransfer).touchAction('tap')
-        await (await this.roadSideAssistance).touchAction('tap')
-    }
-    async selectHowQuickly() {
-        await (await this.withinFourDay).click()
-    }
-    async exchangeVehicle() {
-        await (await this.exchange).click()
-    }
-    async Enterkms() {
-        await (await this.kms).setValue("10000")
-    }
-    async closePopUp()
-    {
-        await (await this.closeBtn).click()
-    }
+
 }
 module.exports = new Usedcar();
