db2c4a90fed41fa8370c0c097e12a3a105d7b48c
punamvtest <poonam@vtestcorp.com>
2021-12-24 21:36:24 +0530

    used car Andiod testcases

--- Pages/Android/usedCarPage.js
+++ Pages/Android/usedCarPage.js
@@ -39,7 +39,7 @@ class Usedcar {
     get modeloption() {return base.getByText("TLX")}
     get yearoption() {return base.getByText("2018")}
     get trimoption() {return base.getByText("BASE")}
-    get heading3() {return base.getByText("Select the Vehicle Entering Make, Model, Year & trim")}
+    get heading3() {return base.getByResourceId("in.droom:id/lookingForTV")}
     get selectCar() {return base.getByResourceId("in.droom:id/makeModelYearTrimTV")}
     get addmoreVehicle() {return base.getByResourceId("in.droom:id/addMoreVehicleBTN")}
     get budget() {return base.getByText("What Is Your Budget?")}
@@ -53,62 +53,25 @@ class Usedcar {
     get kmDrivenRange() {return base.getByText("KM Driven Range")}
     get kmSlider() {return base.getByResourceId("in.droom:id/kmDrivenSlider")}
     get extColor() {return base.getByText("Select Exterior Color")}
+    get color1() {return base.getByResourceId("in.droom:id/smb_CardCV")}
+    get transmissionType() {return base.getByResourceId("in.droom:id/transmissionTypeTV")}
+    get type1() {return base.getByResourceId("in.droom:id/autoCompleteTextView")}
+    get lastStep() {return base.getByText("Last Step to Go! Few More Details")}
+    get doyouNeed() {return base.getByResourceId("in.droom:id/txt_are_you_flexible_for")}
+    get insurance() {return base.getByResourceId("in.droom:id/txt_insurance")}
+    get loan() {return base.getByResourceId("in.droom:id/txt_smb_loan")}
+    get loantoggle() {return base.getByResourceId("in.droom:id/loan_toggle")}
+    get rcTransfer() {return base.getByResourceId("in.droom:id/txt_rc_transfer")}
+    get roadAssistance() {return base.getByResourceId("in.droom:id/txt_road_side_assitance")}
+    get quickBuy() {return base.getByResourceId("in.droom:id/txt_how_quickly_do_you_want_to_buy")}
+    get weeks() {return base.getByResourceId("in.droom:id/radio_one_four_week")}
+    get exchange() {return base.getByResourceId("in.droom:id/txt_do_you_wish_to_exchange_your_vehicle")}
+    get exchangetoggle() {return base.getByResourceId("in.droom:id/yes_toggle")}
+    get submitheading() {return base.getByText("Great! More Information Gives Best Results")}
+    get almostdone() {return base.getByText("You are Almost Done!")}
+    get reqCount() {return base.getByText("We found 0 matches as per your requirement")}
 
-   /*// get myReq() { return base.getByResourceId("in.droom:id/textview_my_requirement") }
-  //  get newRequirements() { return base.getByResourceId("in.droom:id/addNewRequirement") }
-   // get car() { return base.getByResourceId("in.droom:id/carLayout") }
-    get usedButton() { return base.getByResourceId("in.droom:id/usedRadioButton") }
-    get bike() { return base.getByResourceId("in.droom:id/bikeLayout") }
-    get scooter() { return base.getByResourceId("in.droom:id/scooterLayout") }
-   // get carimg() { return base.getByResourceId("in.droom:id/carLayout") }
-    get newButton() { return base.getByResourceId("in.droom:id/newRadioButton") }
-    get location() { return base.getByResourceIdNText("in.droom:id/autoCompleteEditText","Select Location") }
-    get nextButton() { return base.getByText("Next") }
-    get currentLocation() { return base.getByResourceId("in.droom:id/detectLocationTV")}
-    get errorMsg() { return base.getByText("Please select location")}
-    get vehicleDetails() { return base.getByResourceId("in.droom:id/txt_enter_vehicle_details") }
-    get swift() { return base.getByResourceId("in.droom:id/search_view") }
-    get selectSwift() { return base.getByTextContains("Maruti Suzuki") }
-    get carName() { return base.getByResourceId("in.droom:id/makeModelYearTrimTV")}
-    get addBtn() { return base.getByResourceId("in.droom:id/addMoreVehicleBTN") }
-    get proSellerCount() { return base.getByResourceId("in.droom:id/textView171") }
-    get vehicleCount() { return base.getByTextContains("Vehicle Found in")}
-    get closeRecord() { return base.getByResourceId("in.droom:id/deleteVehicleIV")}
-    get selectFrom() { return base.getByResourceId("in.droom:id/selectManuallyTV") }
-    get orSearch() { return base.getByResourceId("in.droom:id/searchTV") }
-    get make() { return base.getByResourceId("in.droom:id/sp_buy_make")}
-    get selectMake() { return base.getByResourceId("in.droom:id/tv_tinted_spinner")}
-    get model() { return base.getByResourceId("in.droom:id/sp_buy_model")}
-    get selectModel() { return base.getByResourceId("in.droom:id/tv_tinted_spinner")}
-    get year() { return base.getByResourceId("in.droom:id/sp_buy_year")}
-    get selectyear() { return base.getByResourceId("in.droom:id/tv_tinted_spinner") }
-    get trim() { return base.getByResourceId("in.droom:id/sp_buy_trim") }
-    get selectTrim() { return base.getByResourceId("in.droom:id/tv_tinted_spinner") }
-    get submitR() { return base.getByResourceId("in.droom:id/menu_my_requirement")}
-    get droomSugg() { return base.getByResourceId("in.droom:id/droomSuggestBTN") }
-    get lookingFor() { return base.getByText("Price Range") }
-    get priceRange() { return base.getByTextContains("Price Range")}
-    get body() { return base.getByText("Body Type") }
-    get age() { return base.getByText("Year Range") }
-   // get budget() { return base.getByText("What Is Your Budget?") }
-    get fuelType() { return base.getByText("Fuel Type") }
-    get petrol() { return base.getByClassNIndex("android.widget.Switch","0") }
-    get lastStep() { return base.getByResourceIdNIndex("in.droom:id/headingTextView","2") }
-    get insurance() { return base.getByResourceId("in.droom:id/insurance_toggle") }
-    get loan() { return base.getByResourceId("in.droom:id/loan_toggle") }
-    get RCTransfer() { return base.getByResourceId("in.droom:id/rc_transfer_toggle")}
-    get roadSideAssistance() { return base.getByResourceId("in.droom:id/road_side_assitance_toggle")}
-    get almostDone() { return base.getByText("You are Almost Done!") }
-    get submitAndView() { return base.getByText("Submit & View all Matches")}
-    get requirements() { return base.getByResourceId("in.droom:id/viewRequirements") }
-    get moreBtn() {return base.getByResourceId("in.droom:id/txt_more")}
-    get fuleTypes() { return base.getByResourceId("in.droom:id/petrol_view") }
-    get redColor() { return base.getByResourceIdNIndex("in.droom:id/smb_CardCV","0") }
-    get extColor() { return base.getByResourceIdNText("in.droom:id/txt_km_driven","Red") }
-    get transmissionType() { return base.getByResourceIdNText("in.droom:id/txt_km_driven","Automatic") }
-    get automatic() { return base.getByResourceIdNIndex("in.droom:id/icon_card_view","0") }
-    get flexibleTransmission() { return base.getByResourceId("in.droom:id/radio_flexible_for_transmission_RV") }
-    */
+   
     
    
     async droomSuggestCar(){
