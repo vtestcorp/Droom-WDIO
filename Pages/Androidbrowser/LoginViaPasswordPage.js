a4099c9479585ca90951e8e42033c397c4bd4609
vtestcorp1@gmail.com <vtestcorp1@gmail.com>
2021-12-30 18:10:26 +0530

    msite files for search and Login via password

---
+++ Pages/Androidbrowser/LoginViaPasswordPage.js
@@ -0,0 +1,22 @@
+const base = require("../../BaseClass/base");
+class Login {
+    get menu(){return base.getByText("Menu") }
+    get login(){return base.getByText("Login")}
+    get loginToAccount(){return base.getByText("Login to Your Account")}
+    get loginwithemailBtn(){return base.getByText("Email Login with Email")}
+    get emailField(){return base.getByResourceId("email")}
+    get passwordField(){return base.getByResourceId("password")}
+    get Forgotpassword(){return base.getByText("Forgot Password")}
+    get continueBtn(){return base.getByResourceId("continueEmail")}
+    get myAccount(){return base.getByText("My Account")}
+    get allowBtn(){return base.getByText("Allow")}
+    get blockBtn(){return base.getByText("Block")}
+    get errorMsgForInvalidPasswd(){return base.getByText("Your username or password is incorrect")}
+    get errorMsgForInvalidId(){return base.getByText("User does not exist")}
+    get errorMsgForBlankID(){return base.getByText("Please enter a valid Email address.")}
+    get errorMsgForBlankPass(){return base.getByText("Please enter a password.")}
+    get loginWithFacebook(){return base.getByContDesc("Refresh Login with Facebook")}
+    get faceBookPage(){return base.getByText("Log in to your Facebook account to connect to Droom")}
+    
+}
+module.exports = new Login();
