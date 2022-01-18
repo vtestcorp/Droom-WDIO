6f36a56c36fc66ee58ee3f53d698b5bd7c6011f1
punamvtest <poonam@vtestcorp.com>
2022-01-18 15:13:24 +0530

    changes in search test

--- Pages/Web/ForgotPasswordPage.js
+++ Pages/Web/ForgotPasswordPage.js
@@ -1,15 +1,16 @@
 class ForgotPassword {
-    get popup() { return $('.jss1') }
-    get popupclose() { return $('(//button[@class="close em-show-later"])[2]') }
+    get popup() { return $('(//div[@class="modal-body"])[23]') }
+    get popupclose() { return $('#detectMyLocation') }
     get profileicon() { return $('#user_details') }
-    get loginBtn() { return $('//a[text()="Log In"]') }
-    get emailTB() { return $('#userLogin') }
+    get loginBtn() { return $('a[class="btn btn-primary btn-block"]') }
+    get loginwithemail() {return $('(//button[@id="loginWithEmail"])')}
+    get emailTB() { return $('#email') }
     get sellerName() { return $('(//div[@class="seller-name"])[2]') }
     get loginViaPasswordBtn() { return $('.d-letter-spacing-1[href="#viaPassword"]') }
-    get forgotPasswordLink() { return $('//a[text()="Forgot Your Password?"]') }
+    get forgotPasswordLink() { return $('//a[text()="Forgot Password"]') }
     get loginwitholdPassword() { return $('//a[text()="Log In With Old Password "]') }
     get passwordTB() { return $('#password') }
-    get login() { return $('//input[@type="submit"]') }
+    get login() { return $('(//button[@id="continueEmail"])') }
 
     async loginViaPassword() {
         console.log('User Launch URL')
@@ -17,6 +18,9 @@ class ForgotPassword {
         console.log('User move to profile')
         await (await this.loginBtn).click()
         console.log('User click on login button')
+        await (await this.loginwithemail).click()
+        console.log('User click on login button')
+
     }
 }
 module.exports = new ForgotPassword();
